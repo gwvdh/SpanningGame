02ee0446edb20e77023ac739a21b12c48bf42c8f
14138650 <gwvdh97@gmail.com>
2024-10-05 10:11:39 +0200

    Refactor scoreboard

--- scoreboard.py
+++ scoreboard.py
@@ -1,31 +1,31 @@
-from flask import Flask, render_template, jsonify, request
-import requests
+from flask import Flask, render_template
 
 app = Flask(__name__, template_folder="templates")
 
 
-def get_entries(page):
+def get_entries(types: dict[int, str]):
     dict_keys = ["type", "name", "score", "filename"]
+    scoreboards = {}
     with open ("user_input/entries.txt", "r") as f:
         entries = f.readlines()
         # csv to dict
         entries = [dict(zip(dict_keys, entry.split(","))) for entry in entries]
-        print(f'{entries}')
-        # filter by page
-        entries = [entry for entry in entries if int(entry["type"]) == page]
         for entry in entries:
-            if int(entry["type"]) == 1:
-                entry["type"] = "TUM logo"
-            elif int(entry["type"]) == 2:
-                entry["type"] = "Spanning tree"
+            if int(entry["type"]) not in scoreboards:
+                if int(entry["type"]) not in types:
+                    continue
+                scoreboards[int(entry["type"])] = {"title": types[int(entry["type"])], "entries": []}
+            scoreboards[int(entry["type"])]["entries"].append(entry)
         # sort by score
-        entries = sorted(entries, key=lambda x: float(x["score"]), reverse=False)
-        return entries
+        for _, scoreboard in scoreboards.items():
+            scoreboard["entries"] = sorted(scoreboard["entries"], key=lambda x: float(x["score"]), reverse=False)
+        scoreboards = [value for _, value in scoreboards.items()]
+        return scoreboards
 
  
 @app.route("/")
 def index():
-    return render_template("index.html", message="Hello World", table_1_entries=get_entries(1), table_2_entries=get_entries(2))
+    return render_template("scoreboard.html", message="Hello World", scoreboards=get_entries({1: "TUM logo", 2: "Spanning tree"}))
  
  
 if __name__ == "__main__":
